bad86f3d32240263cbf9c0f11e1773d6d6b4001b
GreenPine <x@oruby.cn>
2023-04-09 17:00:46 +0800

    新闻增加时效

--- utils/news.py
+++ utils/news.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import requests
+from datetime import date
 import os
 import json
 from serpapi import GoogleSearch
@@ -14,7 +14,7 @@ headers = {
 
 def fetch_news(name):
     # 获取 link
-    key = "news:{}:v1".format(name)
+    key = "news:{}:{}:v1".format(name, date.today())
     data = cache.get(key)
     if data:
         return json.loads(data)
